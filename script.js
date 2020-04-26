6369eb118a5c9b266019d4bc1d791427bddc5162
Pedro Souza <pedrohsouzadf@gmail.com>
2020-04-26 16:40:16 -0300

    criando snake

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box =32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 function criarBG(){
     context.fillStyle = "lightgreen";
@@ -8,4 +13,12 @@ function criarBG(){
 
 }
 
+function criarSnake(){
+    for(i=0; i < snake.length; i++){
+        context.fillStyle = "blue";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+
+}
 criarBG();
+criarSnake();
